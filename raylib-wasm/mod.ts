1b57587b415c0a29ff7dac49e1b35ea4b19bbac4
Ilya Siganov <ilya.blan4@gmail.com>
2024-09-25 11:01:09 +0600

    feat: wrap raylib wasm module as Raylib class

--- raylib-wasm/mod.ts
+++ raylib-wasm/mod.ts
@@ -1,19 +1,28 @@
 // @ts-types="./build/raylibjs.d.ts"
 import initModule, { type MainModule } from "./build/raylibjs.js";
 
-// @ts-types="./build/raylibjs.d.ts"
-export type * from "./build/raylibjs.js";
+export default class Raylib {
+  constructor(public readonly mod: MainModule) {}
 
-/**
- * Initialize Raylib bindings: canvas, wasm.
- *
- * @param root canvas where game should be rendered in
- * @returns raylib api object
- */
-export async function initRaylib(root: HTMLCanvasElement): Promise<MainModule> {
-  const mod = await initModule({
-    canvas: root,
-  });
-  // TODO: rewrap bindings or add docs
-  return mod;
+  /**
+   * Initialize Raylib bindings: canvas, wasm.
+   *
+   * @param options wasm module factory options
+   * @returns raylib api object
+   */
+  static async initRaylib(
+    options: { canvas: HTMLCanvasElement },
+  ): Promise<Raylib> {
+    const mod = await initModule(options);
+    return new Raylib(mod);
+  }
+
+  /**
+   * Initialize window and OpenGL context
+   * @param width
+   * @param height
+   */
+  InitWindow(width: number, height: number, title: string): void {
+    this.mod.InitWindow(width & 0xFFFFFFFF, height & 0xFFFFFFFF, title);
+  }
 }
